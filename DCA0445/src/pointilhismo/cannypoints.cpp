b6b4ae809dbe29162464c21e05f3b0a52f025830
Denis <dnsricardo@gmail.com>
2017-05-06 12:18:24 -0300

    Pointilhismo funcionando com escala de cinza.

--- DCA0445/src/pointilhismo/cannypoints.cpp
+++ DCA0445/src/pointilhismo/cannypoints.cpp
@@ -11,8 +11,17 @@
 using namespace std;
 using namespace cv;
 
-int top_slider = 10;
-int top_slider_max = 200;
+int threshold_slider = 10;
+int threshold_slider_max = 200;
+
+int step_slider = 1;
+int step_slider_max = 10;
+
+int jitter_slider = 3;
+int jitter_slider_max = 10;
+
+int raio_slider = 2;
+int raio_slider_max = 10;
 
 #define STEP 2
 #define JITTER 3
@@ -22,8 +31,9 @@ int top_slider_max = 200;
  * pelo algoritmo de Canny. */
 Mat image, border, points, resultado;
 
-void on_trackbar_canny(int, void*){
-  Canny(image, border, top_slider, 3*top_slider);
+void alterarSliderCanny(int, void*)
+{
+  Canny(image, border, threshold_slider, 3*threshold_slider);
   uchar gray;
   resultado = points.clone();
   
@@ -47,20 +57,75 @@ void on_trackbar_canny(int, void*){
     }
    }
   
-  imshow("resultado", resultado);
   imshow("canny", border);
+  imshow("resultado", resultado);
+  
 }
+
+
+
+void alterarSliderPointilhismo(int, void*)
+{
+    vector<int> yrange;
+    vector<int> xrange;
+    int width, height, gray;
+    int x, y;
+    srand(time(0));
+   
+       width = image.cols;
+       height = image.rows;
+
+       xrange.resize(height/step_slider);
+       yrange.resize(width/step_slider);
+        
+       /* Preenche ambos os vetores com 0's. */
+       iota(xrange.begin(), xrange.end(), 0);
+       iota(yrange.begin(), yrange.end(), 0);
+
+      for(uint i = 0; i < xrange.size(); i++){
+        xrange[i]= xrange[i] *step_slider + step_slider/2;
+      }
+
+      for(uint i = 0; i < yrange.size(); i++){
+        yrange[i]= yrange[i]*step_slider+step_slider/2;
+      }
+
+      points = Mat(height, width, CV_8U, Scalar(255));
+
+      random_shuffle(xrange.begin(), xrange.end());
+
+      for(auto i : xrange){
+        random_shuffle(yrange.begin(), yrange.end());
+        for(auto j : yrange){
+          x = i+rand()%(2*jitter_slider)-jitter_slider+1;
+          y = j+rand()%(2*jitter_slider)-jitter_slider+1;
+          gray = image.at<uchar>(x,y);
+          circle(points,
+                 cv::Point(y,x),
+                 raio_slider,
+                 CV_RGB(gray,gray,gray),
+                 -1,
+                 CV_AA);
+        }
+      }
+    
+    
+    imshow("pointilhismo", points);
+    
+    alterarSliderCanny(0, 0);
+    imshow("resultado", resultado);
+}
+
+
 
 int main(int argc, char**argv)
 {
 
-  vector<int> yrange;
-  vector<int> xrange;
+
 
     /* Matriz points guarda o resultado dos circulos. */
 
-  int width, height, gray;
-  int x, y;
+
 
     /* Verifica o número de argumentos.  */
     if (argc != 2) 
@@ -72,7 +137,10 @@ int main(int argc, char**argv)
     
     /* Checa se a imagem pode ser aberta. */
     image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-
+    resultado = image.clone();
+    points = image.clone();
+    border = image.clone();
+    
     if (!image.data) 
     {
         cout << "A imagem não pode ser aberta." << endl;
@@ -80,57 +148,40 @@ int main(int argc, char**argv)
     }
     
     namedWindow("canny", 1);
+    namedWindow("pointilhismo", 1);
+    namedWindow("resultado", 1);
     
-    Canny(image, border, top_slider, 3*top_slider);
+    Canny(image, border, threshold_slider, 3*threshold_slider);
 
     createTrackbar("Treshold inferior", "canny",
-                &top_slider,
-                top_slider_max,
-                on_trackbar_canny );
+                &threshold_slider,
+                threshold_slider_max,
+                alterarSliderCanny);
+                
+    createTrackbar("Step", "pointilhismo",
+                &step_slider,
+                step_slider_max,
+                alterarSliderPointilhismo);
+                
+    createTrackbar("Jitter", "pointilhismo",
+            &jitter_slider,
+            jitter_slider_max,
+            alterarSliderPointilhismo);
+            
+    createTrackbar("Raio", "pointilhismo",
+            &raio_slider,
+            raio_slider_max,
+            alterarSliderPointilhismo);
 
    
    /* aplica o efeito do pointilhismo. */
    
-   srand(time(0));
-   
-   width = image.cols;
-   height = image.rows;
-
-   xrange.resize(height/STEP);
-   yrange.resize(width/STEP);
+  
     
-   /* Preenche ambos os vetores com 0's. */
-   iota(xrange.begin(), xrange.end(), 0);
-   iota(yrange.begin(), yrange.end(), 0);
-
-  for(uint i = 0; i < xrange.size(); i++){
-    xrange[i]= xrange[i] *STEP + STEP/2;
-  }
-
-  for(uint i = 0; i < yrange.size(); i++){
-    yrange[i]= yrange[i]*STEP+STEP/2;
-  }
-
-  points = Mat(height, width, CV_8U, Scalar(255));
-
-  random_shuffle(xrange.begin(), xrange.end());
-
-  for(auto i : xrange){
-    random_shuffle(yrange.begin(), yrange.end());
-    for(auto j : yrange){
-      x = i+rand()%(2*JITTER)-JITTER+1;
-      y = j+rand()%(2*JITTER)-JITTER+1;
-      gray = image.at<uchar>(x,y);
-      circle(points,
-             cv::Point(y,x),
-             RAIO,
-             CV_RGB(gray,gray,gray),
-             -1,
-             CV_AA);
-    }
-  }
     
-  on_trackbar_canny(top_slider, 0 );  
+  alterarSliderPointilhismo(0, 0);
+  alterarSliderCanny(threshold_slider, 0 );
+  
   waitKey();
   return 0;
 }
