82ff45840594c4217a5792114976dbd1510f0b6d
Denis <dnsricardo@gmail.com>
2017-06-20 21:59:57 -0300

    Tentativa com Transformada Polar Harmonica.

--- DCA0445/src/ProjetoFinal/ann/classificador.cpp
+++ DCA0445/src/ProjetoFinal/ann/classificador.cpp
@@ -12,29 +12,42 @@ using namespace std;
 #define TIPOS_MOEDAS 8
 #define NUM_AMOSTRAS 60
 
-#define NUM_MI 3
+
+vector<float> mat2vec(const Mat &mat)
+{
+    vector<float> array;
+    if (mat.isContinuous()) {
+      array.assign(mat.datastart, mat.dataend);
+    } else {
+      for (int i = 0; i < mat.rows; ++i) {
+        array.insert(array.end(), mat.ptr<uchar>(i), mat.ptr<uchar>(i)+mat.cols);
+      }
+    }
+    
+    return array;
+}
 
 int main()
 {
 	initModule_nonfree();
     
-    Mat entrada(1, NUM_MI, CV_32FC1);
+    Mat entrada(1, 256, CV_32FC1);
     Mat saida(1, TIPOS_MOEDAS, CV_32FC1);
 	
 	Mat imagem = imread("teste.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    resize(imagem, imagem, Size(400, 400));
-    equalizeHist(imagem, imagem);
-    blur(imagem, imagem, Size(3,3));
-    Canny(imagem, imagem, 100, 200, 3);
+    resize(imagem, imagem, Size(16, 16));
     
-    double momentos[7];
-    Moments m = moments(imagem, false);
-	HuMoments(m, momentos);
-    
-    for(int i = 0; i < NUM_MI; i++)
+    vector<float> d = mat2vec(imagem);
+    for(int i = 0; i < 256; i++)
     {
-        entrada.at<float>(0, i) = momentos[i];
+        entrada.at<float>(0,i) = d[i];
     }
+    
+    
+    //equalizeHist(imagem, imagem);
+    //blur(imagem, imagem, Size(3,3));
+    //Canny(imagem, imagem, 100, 200, 3);
+    
 
 	
 	CvANN_MLP mlp;
--- DCA0445/src/ProjetoFinal/ann/final.cpp
+++ DCA0445/src/ProjetoFinal/ann/final.cpp
@@ -12,12 +12,28 @@ using namespace std;
 
 // Parâmetros da MLP
 
-#define NUM_MAX_ITER 100000
-#define EPSILON 1e-6f
+#define NUM_MAX_ITER 1000
+#define EPSILON 1e-4f
 #define LEARNING_RATE 0.1f
-#define MOMENTUM 0.7f
+#define MOMENTUM 0.1f
 
 #define NUM_MI 3
+#define CANNY_THRESH 10
+
+vector<float> mat2vec(const Mat &mat)
+{
+    vector<float> array;
+    if (mat.isContinuous()) {
+      array.assign(mat.datastart, mat.dataend);
+    } else {
+      for (int i = 0; i < mat.rows; ++i) {
+        array.insert(array.end(), mat.ptr<uchar>(i), mat.ptr<uchar>(i)+mat.cols);
+      }
+    }
+    
+    return array;
+}
+
 
 void treinar(const Mat &entradas, const Mat& saidas) {
 
@@ -25,7 +41,7 @@ void treinar(const Mat &entradas, const Mat& saidas) {
 	// Definição das camadas.
     Mat camadas = cv::Mat(3, 1, CV_32SC1);
     camadas.row(0) = Scalar(entradas.cols);
-    camadas.row(1) = Scalar((entradas.cols + saidas.cols)/2);
+    camadas.row(1) = Scalar(32);
     camadas.row(2) = Scalar(saidas.cols);
 
 	// Definição dos parâmetros;
@@ -63,7 +79,7 @@ int main()
 	initModule_nonfree();
     
     // Definindo conjunto treinamento.
-    Mat entradas(TIPOS_MOEDAS * NUM_AMOSTRAS, NUM_MI, CV_32FC1);
+    Mat entradas(TIPOS_MOEDAS * NUM_AMOSTRAS, 6400, CV_32FC1);
     Mat saidas = Mat::zeros(TIPOS_MOEDAS * NUM_AMOSTRAS, TIPOS_MOEDAS, CV_32FC1);
     
 	vector<Mat> moedas10f(NUM_AMOSTRAS, Mat());
@@ -75,14 +91,14 @@ int main()
 	vector<Mat> moedas100f(NUM_AMOSTRAS, Mat());
 	vector<Mat> moedas100n(NUM_AMOSTRAS, Mat());
 	
-	vector<vector<double> > dMoedas10f(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas10n(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas25f(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas25n(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas50f(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas50n(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas100f(NUM_AMOSTRAS, vector<double>(7));
-	vector<vector<double> > dMoedas100n(NUM_AMOSTRAS, vector<double>(7));
+	vector<vector<float> > dMoedas10f(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas10n(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas25f(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas25n(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas50f(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas50n(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas100f(NUM_AMOSTRAS, vector<float>(6400, 0));
+	vector<vector<float> >  dMoedas100n(NUM_AMOSTRAS, vector<float>(6400, 0));
 	
 	/* Lê todas as amostras. */
 	for(int i = 0; i < NUM_AMOSTRAS; i++)
@@ -96,106 +112,61 @@ int main()
 		moedas100f[i] = imread("100f/" + to_string(i+1) + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
 		moedas100n[i] = imread("100n/" + to_string(i+1) + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
         
+        resize(moedas10f[i], moedas10f[i], Size(80, 80));
+        resize(moedas10n[i], moedas10n[i], Size(80, 80));
+        resize(moedas25f[i], moedas25f[i], Size(80, 80));
+        resize(moedas25n[i], moedas25n[i], Size(80, 80));
+        resize(moedas50f[i], moedas50f[i], Size(80, 80));
+        resize(moedas50n[i], moedas50n[i], Size(80, 80));
+        resize(moedas100f[i], moedas100f[i], Size(80, 80));
+        resize(moedas100n[i], moedas100n[i], Size(80, 80));
         
-        resize(moedas10f[i], moedas10f[i], Size(32, 32));
-        resize(moedas10n[i], moedas10n[i], Size(32, 32));
-        resize(moedas25f[i], moedas25f[i], Size(32, 32));
-        resize(moedas25n[i], moedas25n[i], Size(32, 32));
-        resize(moedas50f[i], moedas50f[i], Size(32, 32));
-        resize(moedas50n[i], moedas50n[i], Size(32, 32));
-        resize(moedas100f[i], moedas100f[i], Size(32, 32));
-        resize(moedas100n[i], moedas100n[i], Size(32, 32));
-		
-        imshow("e1", moedas100f[i]);
-        waitKey(0);
         
-		equalizeHist(moedas10f[i], moedas10f[i]);
-		equalizeHist(moedas10n[i], moedas10n[i]);
-		equalizeHist(moedas25f[i], moedas25f[i]);
-		equalizeHist(moedas25n[i], moedas25n[i]);
-		equalizeHist(moedas50f[i], moedas50f[i]);
-		equalizeHist(moedas50n[i], moedas50n[i]);
-		equalizeHist(moedas100f[i], moedas100f[i]);
-		equalizeHist(moedas100n[i], moedas100n[i]);
+        IplImage ipl10f = moedas10f[i];
+        IplImage ipl10n = moedas10n[i];
+        IplImage ipl25f = moedas25f[i];
+        IplImage ipl25n = moedas25n[i];
+        IplImage ipl50f = moedas50f[i];
+        IplImage ipl50n = moedas50n[i];
+        IplImage ipl100f = moedas100f[i];
+        IplImage ipl100n = moedas100n[i];
         
-        imshow("e2", moedas100f[i]);
-        waitKey(0);
+        IplImage *p10f = &ipl10f;
+        IplImage *p10n = &ipl10n;
+        IplImage *p25f = &ipl25f;
+        IplImage *p25n = &ipl25n;
+        IplImage *p50f = &ipl50f;
+        IplImage *p50n = &ipl50n;
+        IplImage *p100f = &ipl100f;
+        IplImage *p100n = &ipl100n;
         
-		blur(moedas10f[i], moedas10f[i], Size(3,3));
-		blur(moedas10n[i], moedas10n[i], Size(3,3));
-		blur(moedas25f[i], moedas25f[i], Size(3,3));
-		blur(moedas25n[i], moedas25n[i], Size(3,3));
-		blur(moedas50f[i], moedas50f[i], Size(3,3));
-		blur(moedas50n[i], moedas50n[i], Size(3,3));
-		blur(moedas100f[i], moedas100f[i], Size(3,3));
-		blur(moedas100n[i], moedas100n[i], Size(3,3));
-        
-        imshow("e3", moedas100f[i]);
-        waitKey(0);
-        
-        Canny(moedas10f[i], moedas10f[i], 20, 60, 3);
-        Canny(moedas10n[i], moedas10n[i], 20, 60, 3);
-        Canny(moedas25f[i], moedas25f[i], 20, 60, 3);
-        Canny(moedas25n[i], moedas25n[i], 20, 60, 3);
-        Canny(moedas50f[i], moedas50f[i], 20, 60, 3);
-        Canny(moedas50n[i], moedas50n[i], 20, 60, 3);
-        Canny(moedas100f[i], moedas100f[i], 20, 60, 3);
-        Canny(moedas100n[i], moedas100n[i], 20, 60, 3);
+        cvLinearPolar(p10f, p10f, CvPoint2D32f(Point(40, 40)), 40);
+        cvLinearPolar(p10n, p10n, CvPoint2D32f(Point(40, 40)), 40);
+        cvLinearPolar(p25f, p25f, CvPoint2D32f(Point(40, 40)), 40);
+        cvLinearPolar(p25n, p25n, CvPoint2D32f(Point(40, 40)), 40);        
+        cvLinearPolar(p50f, p50f, CvPoint2D32f(Point(40, 40)), 40);
+        cvLinearPolar(p50n, p50n, CvPoint2D32f(Point(40, 40)), 40);
+        cvLinearPolar(p100f, p100f, CvPoint2D32f(Point(40, 40)), 40);
+        cvLinearPolar(p100n, p100n, CvPoint2D32f(Point(40, 40)), 40);
 
-        imshow("e4", moedas100f[i]);
-        waitKey(0);
-
-	}
-	
-	// Calcula descritores para as imagens de amostra. 
-	for(int i = 0; i < NUM_AMOSTRAS; i++)
-	{
-        double mi10f[7];
-        double mi10n[7];
-        double mi25f[7];
-        double mi25n[7];
-        double mi50f[7];
-        double mi50n[7];
-        double mi100f[7];
-        double mi100n[7];
-        
-        Moments mom10f = moments(moedas10f[i], false);
-        Moments mom10n = moments(moedas10n[i], false);
-        Moments mom25f = moments(moedas25f[i], false);
-        Moments mom25n = moments(moedas25n[i], false);
-        Moments mom50f = moments(moedas50f[i], false);
-        Moments mom50n = moments(moedas50n[i], false);
-        Moments mom100f = moments(moedas100f[i], false);
-        Moments mom100n = moments(moedas100n[i], false);
-        
-        HuMoments(mom10f, mi10f);
-        HuMoments(mom10n, mi10n);
-        HuMoments(mom25f, mi25f);
-        HuMoments(mom25n, mi25n);
-        HuMoments(mom50f, mi50f);
-        HuMoments(mom50n, mi50n);
-        HuMoments(mom100f, mi100f);
-        HuMoments(mom100n, mi100n);
-        
-        for(int j = 0; j < NUM_MI; j++)
-        {
-            dMoedas10f[i][j] = mi10f[j];
-            dMoedas10n[i][j] = mi10n[j];
-            dMoedas25f[i][j] = mi25f[j];
-            dMoedas25n[i][j] = mi25n[j];
-            dMoedas50f[i][j] = mi50f[j];
-            dMoedas50n[i][j] = mi50n[j];
-            dMoedas100f[i][j] = mi100f[j];
-            dMoedas100n[i][j] = mi100n[j];
-            
-        }
-        
 	}
     
+    for(int i = 0; i < NUM_AMOSTRAS; i++)
+    {
+        dMoedas10f[i] = mat2vec(moedas10f[i]);
+        dMoedas10n[i] = mat2vec(moedas10n[i]);
+        dMoedas25f[i] = mat2vec(moedas25f[i]);
+        dMoedas25n[i] = mat2vec(moedas25n[i]);
+        dMoedas50f[i] = mat2vec(moedas50f[i]);
+        dMoedas50n[i] = mat2vec(moedas50n[i]);
+        dMoedas100f[i] = mat2vec(moedas100f[i]);
+        dMoedas100n[i] = mat2vec(moedas100n[i]);
+    }
+	    
     
     for(int i = 0; i < TIPOS_MOEDAS * NUM_AMOSTRAS; i += TIPOS_MOEDAS)
     {
-        for(int j = 0; j < NUM_MI; j++)
+        for(int j = 0; j < 6400; j++)
         {
             entradas.at<float>(i,j) = dMoedas10f[i/TIPOS_MOEDAS][j];
             entradas.at<float>(i+1,j) = dMoedas10n[i/TIPOS_MOEDAS][j];
@@ -220,11 +191,10 @@ int main()
     
     treinar(entradas, saidas);
     
-    /*
 	Mat saidasMLP(TIPOS_MOEDAS * NUM_AMOSTRAS, TIPOS_MOEDAS, CV_32FC1);
 	
 	CvANN_MLP mlp;
-	mlp.load("pesos.yml", "mlp");
+	mlp.load("mlp.yml", "mlp");
 	
 	mlp.predict(entradas, saidasMLP);
 
@@ -236,8 +206,7 @@ int main()
         }
         cout <<  "----------------------------------------" << endl;
     }
-    * */
-	
-	
+
+
 		
 }
